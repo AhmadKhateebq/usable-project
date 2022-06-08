fa55d991e48cc2e208ee7e49c61743fd83a663d2
AhmadKhateebq <AhmadKhateebq@gmail.com>
2022-06-08 19:46:40 +0300

    update

--- RedirectServer/src/main/java/com/example/redirectserver/controller/transmitController/OrderController.java
+++ RedirectServer/src/main/java/com/example/redirectserver/controller/transmitController/OrderController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/category")
+@RequestMapping("/api/order")
 public class OrderController {
     @Autowired
     Client client;
